d61b570cf16624aeb56f646f73a5d038001eb3c7
oudeismetis <edward@thinknimble.com>
2024-07-18 14:53:21 -0400

    pass config with slice call

--- server/limitless/projects/tasks.py
+++ server/limitless/projects/tasks.py
@@ -8,14 +8,19 @@ logger = logging.getLogger(__name__)
 ROOT = "/tmp"
 
 
-def slice_model(project):
-    logger.info(f"About to slice model at location: {project.model_3d.url}")
-    response = requests.get(project.model_3d.url)
-    with open(f"{ROOT}/{project.model_3d.name}", mode="wb") as f:
+def _download_file(url, name):
+    logger.info(f"Downloading file: {url}")
+    response = requests.get(url)
+    with open(f"{ROOT}/{name}", mode="wb") as f:
         f.write(response.content)
+
+
+def slice_model(project):
+    _download_file(project.model_3d.url, project.model_3d.name)
+    _download_file(project.print_config.url, project.print_config.name)
     ls = run_command("", "ls -la")
     logger.info(f"files look like: {ls}")
-    output = run_command("", f"CuraEngine slice -l {project.model_3d.name}")
+    output = run_command("", f"CuraEngine slice -v -p -l {project.model_3d.name} -j {project.print_config.name} -o output.gcode")
     logger.info(f"Cura response: {output}")
 
 
