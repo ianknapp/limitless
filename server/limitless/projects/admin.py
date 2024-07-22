cf6abe062ad1983374e672e4cb7b0f3676106dc0
Edward Romano <oudeismetis@users.noreply.github.com>
2024-07-22 17:50:59 -0400

    Ultimaker Cura server install (#5)

--- server/limitless/projects/admin.py
+++ server/limitless/projects/admin.py
@@ -1,8 +1,35 @@
+import logging
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Project
+from .tasks import slice_model
+
+logger = logging.getLogger(__name__)
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
+    change_form_template = "admin/build_project_form.html"
     list_display = ("owner", "title", "model_3d", "print_config")
+
+    def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        if not request.path.endswith("/add/"):
+            # Only allow for slicing after creation
+            extra_context["build_project"] = True
+        return super().changeform_view(request, object_id, form_url, extra_context)
+
+    def response_change(self, request, obj):
+        if "_build_project" in request.POST:
+            file_path = slice_model(obj)
+
+            logger.info(f"Returning file: {file_path.name}")
+            file_data = {}
+            with open(file_path, "rb") as f:
+                file_data = f.read()
+            response = HttpResponse(file_data, content_type="application/gcode")
+            response["Content-Disposition"] = f'attachment; filename="{file_path.name}"'
+            return response
+        return super().response_change(request, obj)
---
+++ server/limitless/projects/tasks.py
@@ -0,0 +1,51 @@
+import logging
+import subprocess
+from pathlib import Path
+
+import requests
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+ROOT = "/tmp"
+
+
+def _download_file(file_path, name):
+    if not file_path.startswith("http"):
+        # Local path; File already on the machine
+        return Path(settings.MEDIA_ROOT, file_path.split("/")[-1])
+    logger.info(f"Downloading file: {file_path}")
+    response = requests.get(file_path)
+    with open(f"{ROOT}/{name}", mode="wb") as f:
+        f.write(response.content)
+    return f"{ROOT}/{name}"
+
+
+def _get_file_name_root(file_name):
+    """
+    We save all files like {name}_2024-07-17T192517.{ext}
+    The timestamp has a fixed length, so just trim it off
+    """
+    return file_name.split(".")[0][:-18]
+
+
+def slice_model(project):
+    _download_file(project.model_3d.url, project.model_3d.name)
+    _download_file(project.print_config.url, project.print_config.name)
+    ls = run_command("", "ls -la")
+    logger.info(f"files look like: {ls}")
+    file_name_root = _get_file_name_root(project.model_3d.name)
+    file_name = f"{file_name_root}.gcode"
+    # Need to export this here for reasons. See buildpack-run.sh
+    export_cmd = "export CURA_ENGINE_SEARCH_PATH=/app/Cura-$(cat /app/cura_version.txt)/resources/definitions"
+    cura_args = f"-j {project.print_config.name} -l {project.model_3d.name} -o /tmp/{file_name}"
+    run_command("", f"{export_cmd} && CuraEngine slice {cura_args}")
+    return Path(ROOT, file_name)
+
+
+def run_command(folder, command):
+    code_root = f"{ROOT}/{folder}"
+    response = subprocess.run(command, cwd=code_root, capture_output=True, shell=True)
+    if response.returncode or response.stderr:
+        logger.info(f"Error while running '{command}' - {response.stdout.decode('UTF-8')} - {response.stderr.decode('UTF-8')}")
+    return response.stdout.decode("UTF-8").rstrip("\n")
