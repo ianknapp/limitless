b063629fca4b6923707bdf472604347a939603cd
oudeismetis <edward@thinknimble.com>
2024-08-29 15:06:57 -0400

    last tweak I hope

--- client/tests/e2e/specs/test-login.cy.ts
+++ client/tests/e2e/specs/test-login.cy.ts
@@ -9,6 +9,6 @@ describe('Tests login workflow', () => {
     cy.get('[data-cy=email]').type(Cypress.env('TEST_USER_EMAIL'))
     cy.get('[data-cy=password]').type(Cypress.env('TEST_USER_PASS'))
     cy.contains('[data-cy=submit]', 'Log In').click()
-    cy.url().should('include', '/home')
+    cy.url().should('include', '/')
   })
 })
