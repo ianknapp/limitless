c0006572bb9e239a65109ade87641cc8a42c252b
ianknapp <i.knapp@scottautomation.com>
2025-01-06 11:47:16 -0500

    Prettier adjustments

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="top-0 z-10 shadow sm:pt-10">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div class="flex flex-row items-center justify-between h-16">
+      <div class="flex flex-row justify-end sm:justify-between h-16">
+        <!-- Left section with nav toggle and search -->
         <div class="flex items-center">
           <div class="content-center hidden sm:inline-block">
             <img
@@ -19,11 +20,13 @@
               @click="toggleLeftNav"
             />
           </div>
+          <!-- Add SearchBar here -->
           <div class="hidden sm:block">
             <SearchBar />
           </div>
         </div>
 
+        <!-- Right section with login/profile -->
         <div class="hidden sm:ml-6 sm:flex sm:items-center">
           <template v-if="!isLoggedIn">
             <router-link :to="{ name: 'Login' }" class="btn--primary bg-zinc-900" data-cy="login"
@@ -85,8 +88,9 @@
         </div>
       </div>
     </div>
-
+    <!-- Mobile menu -->
     <div class="sm:hidden font-sans bg-gray-500/10" v-if="mobileMenuOpen">
+      <!-- Add SearchBar to mobile menu -->
       <div class="px-2 pt-2 pb-3">
         <SearchBar />
       </div>
@@ -167,9 +171,9 @@ export default {
   setup(props, context) {
     const store = useStore()
     const router = useRouter()
-    const leftNavOpen = ref(true)
-    const mobileMenuOpen = ref(false)
-    const profileMenuOpen = ref(false)
+    let leftNavOpen = ref(true)
+    let mobileMenuOpen = ref(false)
+    let profileMenuOpen = ref(false)
 
     async function logout() {
       try {
@@ -195,8 +199,6 @@ export default {
     return {
       toggleLeftNav,
       leftNavOpen,
-      mobileMenuOpen,
-      profileMenuOpen,
       logout,
       isLoggedIn: computed(() => store.getters.isLoggedIn),
       user: computed(() => store.getters.user),
